662606fb389602524f81a7f8b8d0dc6425b68be4
Greg Roberts <groberts3104@gmail.com>
2020-01-03 21:35:51 -0500

    added route to read db.json file

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+    return res.json(db.json);
+  });
+  
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
