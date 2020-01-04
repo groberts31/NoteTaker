4b3aac39bd887ddffe3e9c583d957403762da6b5
Greg Roberts <groberts3104@gmail.com>
2020-01-03 21:30:59 -0500

    added path module and installed it

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var fs = require("fs");
+var path = require("path");
+
 
 var app = express();
 var PORT = 3000;
@@ -13,4 +15,8 @@ app.get("/", function (req, res) {
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
