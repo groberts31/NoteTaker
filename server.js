8efe2afc9046ac02ac11e18358a8b09048df0286
Greg Roberts <groberts3104@gmail.com>
2020-01-03 20:58:21 -0500

    added notes.html and added route in server.js

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
+  
